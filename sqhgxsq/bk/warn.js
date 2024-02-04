f64fbcabd1c91024d53dd24a80cbc7c07c822b3d
胡胡 <105264481+23ut43q@users.noreply.github.com>
2024-02-04 21:28:33 +0800

    2

---
+++ sqhgxsq/bk/warn.js
@@ -0,0 +1,8 @@
+function showDialog() {
+  let dialog = document.getElementById("myDialog");
+  dialog.style.display = "block";
+}
+function closeDialog() {
+  let dialog = document.getElementById("myDialog");
+  dialog.style.display = "none";
+}
